0bb12ac2e72cd69715d090b7749046d69bd4e980
RizDev <rizvan.ahmedov@outlook.com>
2023-03-10 20:43:57 +0400

    Fixed an error due to which the number of completed tasks was not shown on the progress chart
    Fixed an error due to which the number of completed tasks was not shown on the progress chart

--- src/Pages/main-page/MainContent/Chart/CustomTooltip.jsx
+++ src/Pages/main-page/MainContent/Chart/CustomTooltip.jsx
@@ -3,9 +3,8 @@ const CustomTooltip = ({ active, payload }) => {
     return (
       <div className='chart__custom-tooltip'>
         <p className='label'>{payload[0].payload.day}</p>
-        <p className='label'>
-          {payload[0].dataKey} : {payload[0].value}
-        </p>
+        <p className='label'>{`${payload[0].dataKey} : ${payload[0].value}`}</p>
+        <p className='label'>{`${payload[1].dataKey} : ${payload[1].value}`}</p>
       </div>
     )
   }
