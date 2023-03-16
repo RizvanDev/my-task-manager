59ae71219936918ea6da0703e67beb5c3e03899c
RizDev <rizvan.ahmedov@outlook.com>
2023-03-16 12:50:17 +0400

    redux-devtools-extension instaled

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
+import { createStore } from 'redux'
 import App from './App/App.jsx'
 import './helpStyles/reset.scss'
+
+const defaultState = {
+  cash: 0,
+}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      return { ...state, cash: state.cash + action.payload }
+    case 'GET_CASH':
+      return { ...state, cash: state.cash - action.payload }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>,
 )
